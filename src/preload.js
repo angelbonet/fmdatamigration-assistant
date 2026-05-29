d9d8b7ba013186a0179c6a9eaef8b69e469e9c74
angelbonet <angel.bonet@gmail.com>
2026-05-29 07:42:24 +0200

    Add preload bridge

---
+++ src/preload.js
@@ -0,0 +1,20 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Diálogos de selección
+  browseFile: (opts) => ipcRenderer.invoke('browse-file', opts),
+  browseExecutable: () => ipcRenderer.invoke('browse-executable'),
+  browseFolder: () => ipcRenderer.invoke('browse-folder'),
+
+  // Ejecución
+  runMigration: (args) => ipcRenderer.invoke('run-migration', args),
+  cancelMigration: () => ipcRenderer.invoke('cancel-migration'),
+  onMigrationLog: (callback) => ipcRenderer.on('migration-log', (event, data) => callback(data)),
+  removeMigrationLogListener: () => ipcRenderer.removeAllListeners('migration-log'),
+
+  // Script
+  saveScript: (content) => ipcRenderer.invoke('save-script', content),
+
+  // Info
+  getPlatform: () => ipcRenderer.invoke('get-platform'),
+});
